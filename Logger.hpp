05662766ffc20103379d50f38798a629f47fdcd4
chillpert <dev@christianhilpert.com>
2020-09-14 00:49:49 +0200

    cleaned repo

--- Logger.hpp
+++ Logger.hpp
@@ -5,6 +5,29 @@
 // Include this header to your project, but do not forget to compile the source file. 
 // C++17 is required.
 
+// EXAMPLE
+/*
+#include "Logger.hpp"
+
+int main( )
+{
+  lg::verbose( "Apparently, ", 1, " + ", 2, " = ", 1 + 2 );
+  lg::info( "Ah, okay. Thank you for telling me!" );
+  lg::success( "Are you really telling me it worked?" );
+  lg::warning( "I guess I need to be careful now." );
+  lg::error( "A wild error appeared! Luckily, it's not fatal." );
+
+  try
+  {
+    lg::fatal( "Oh my god! I am panicking. I am about to throw a runtime error." );  
+  }
+  catch( ... )
+  {
+    lg::success( "Haha, caught ya!" );
+  }  
+}
+*/
+
 #include <sstream>
 
 #define LOGGER_NAMESPACE lg
